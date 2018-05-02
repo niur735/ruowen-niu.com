cc0d677b24f756f88c9750d0b4c59f4e1328475e
Verity <verity0821@yahoo.com>
2018-05-02 15:16:16 -0400

    change to singing doll

--- content/web/singdoll/assets/js/app.js
+++ content/web/singdoll/assets/js/app.js
@@ -1,7 +1,64 @@
+$(".doll1").mouseover(function() {
+  $(".doll1").css('opacity','1');
+});
+$(".doll1").mouseout(function() {
+  $(".doll1").css('opacity','0');
+});
+
+$(".doll2").mouseover(function() {
+  $(".doll2").css('opacity','1');
+});
+$(".doll2").mouseout(function() {
+  $(".doll2").css('opacity','0');
+});
+
+$(".doll3").mouseover(function() {
+  $(".doll3").css('opacity','1');
+});
+$(".doll3").mouseout(function() {
+  $(".doll3").css('opacity','0');
+});
+
+$(".doll4").mouseover(function() {
+  $(".doll4").css('opacity','1');
+});
+$(".doll4").mouseout(function() {
+  $(".doll4").css('opacity','0');
+});
+
+$(".doll5").mouseover(function() {
+  $(".doll5").css('opacity','1');
+});
+$(".doll5").mouseout(function() {
+  $(".doll5").css('opacity','0');
+});
+
+$(".doll6").mouseover(function() {
+  $(".doll6").css('opacity','1');
+});
+$(".doll6").mouseout(function() {
+  $(".doll6").css('opacity','0');
+});
+
+$(".doll7").mouseover(function() {
+  $(".doll7").css('opacity','1');
+});
+$(".doll7").mouseout(function() {
+  $(".doll7").css('opacity','0');
+});
+
+
+
+
+
+$(document).bind("mousemove", function(e){
+    $(".circle").css({
+       left:  e.pageX - 100,
+       top:   e.pageY - 100
+    });
+});
+
 var number = $(this).data('button');
-
-$('body').click(function(){console.log(number)});
-
 
 var clicked = false;
 
@@ -26,3 +83,27 @@ $(".doll").click(function() {
   else{$(this).removeClass("ondoll"+  $(this).data('button'));
     stand = false;}
 });
+
+$(".open").click(function(){
+  $(".open1").css('left','-50vw');
+  $(".open2").css('right','-50vw');
+});
+
+
+var light = false;
+$(".on").click(function(){
+if(light == false) {
+ $(".curtain").css('background-color','white');
+ $(".lead").html('Turn the light off!');
+ $(".doll").css('opacity','1');
+  light = true;}  
+  else{
+ $(".curtain").css('background-color','black');
+ $(".lead").html('Turn the light on!');
+ $(".doll").css('opacity','0');
+  light = false;}
+});
+
+
+
+
