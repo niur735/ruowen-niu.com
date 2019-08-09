895dafea64867d09061e6f38e33e552bcfdd2cd4
niur735 <31444265+niur735@users.noreply.github.com>
2019-08-09 11:46:53 +0800

    zyw

--- content/research/assets/js/app.js
+++ content/research/assets/js/app.js
@@ -11,11 +11,13 @@ $('.project').hover(
         result = '<div>2017 April <br>This is an essay that compare Ralph Lauren and Calvin Klein in different aspects by knowing their experience.</div>';
     } else if (button == 'studio1-2'){
         result = '<div>2017 April<br>This is an essay that compare Ai Weiwei and Zhang Xiaogang from their life experience and their art styles.</div>';
-    } else if (button == 'introtodesign'){
-        result = '<div>2018 April<br>This is an essay that talks about the design han historical background of the Central Park and how it reflects the material culture</div>';
     } 
+    // else if (button == 'introtodesign'){
+    //     result = '<div>2018 April<br>This is an essay that talks about the design han historical background of the Central Park and how it reflects the material culture</div>';
+    // } 
     else {
-    	result = '<div>2017 July<br>This is essay focus on the how western feminism influence Chinese society. By analysing posters from western society and China around 1990s, author wanted to show the process of how things happened. </div>';
+         result = '<div>2018 April<br>This is an essay that talks about the design han historical background of the Central Park and how it reflects the material culture</div>';
+    	// result = '<div>2017 July<br>This is essay focus on the how western feminism influence Chinese society. By analysing posters from western society and China around 1990s, author wanted to show the process of how things happened. </div>';
     }
 $('.text').html(result);
 });
